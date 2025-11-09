527e35eb171731576b4eb73f46ddcb05ef718898
Santoshi0231-GIT <pandeysan0231@gmail.com>
2025-11-09 13:17:57 +0545

    first commit

---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+const login = () => {
+const [email,setEmail]=useState('');
+const [password,setPassword]=useState('');
+
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default login
