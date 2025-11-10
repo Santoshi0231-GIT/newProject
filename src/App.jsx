3c88fc789b12e483079b8491845eb38449cc0c66
Santoshi0231-GIT <pandeysan0231@gmail.com>
2025-11-10 14:18:24 +0545

    added new data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,58 @@
 import React from 'react'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
+import ForgotPassword from './pages/auth/ForgotPassword'
+import ProtectedRoutes from './component/protected/ProtectedRoutes'
+import Dashboard from './pages/dashboard/Dashboard'
+import NotFound from './pages/notFound/NotFound'
+import { useRoutes } from 'react-router-dom'
+
+
+const appRoutes=[
+  //public routes
+  {
+    path:'/',
+    element:<Login/>
+  },
+  {
+    path:'/register',
+    element:<Register/>
+  },
+  {
+    path:'/forgot-password',
+    element:<ForgotPassword/>
+  },
+
+
+
+  //protected route
+  {
+element:<ProtectedRoutes/>,
+children:[
+  {
+    path:'/dashboard',
+    element:<Dashboard/>
+  }
+]
+  },
+  {
+    path:'*',
+    element:<NotFound/>
+  }
+
+
+
+]
+
+
 
 const App = () => {
+  const routes=useRoutes(appRoutes);
   return (
-    <div>App</div>
+    <div>
+{routes}
+
+    </div>
   )
 }
 
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,14 +1,100 @@
-import React, { useState } from 'react'
 
-const login = () => {
-const [email,setEmail]=useState('');
-const [password,setPassword]=useState('');
+import { useState,useEffect } from "react";
+import { Link,useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/authContext";
 
+function Login() {
+  const { login, error, loading, user } =useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigator = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+
+  };
+
+  console.log(user)
+
+  useEffect(() => {
+    if (user) {
+      navigator("/dashboard", { replace: true });
+    }
+  }, [user, navigator])
 
   return (
-    <>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white p-8 rounded-xl shadow-lg w-[400px]"
+      >
+        <h2 className="text-2xl font-semibold text-center mb-6 text-gray-800">
+          Login
+        </h2>
+
+        {error && (
+          <p className="text-red-600 text-center mb-4 font-medium">{error}</p>
+        )}
+
+
+        <div className="mb-4">
+          <label className="block text-gray-600 mb-1">Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            required
+            className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+
+        <div className="mb-2">
+          <label className="block text-gray-600 mb-1">Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+            required
+            className="w-full p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+        </div>
+        <div className="flex justify-between mb-6">
+          <div >
+            <input type="checkbox" id="rememberMe" className="mr-2"/>
+            <label htmlFor="rememberMe">Remember me</label>
+          </div>
+          <div>
+            <Link to="/forgot-password" className="text-blue-500 curser-pointer">
+              Forgot password?
+            </Link>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          disabled={loading}
+          className={`w-full py-2 rounded-lg text-white font-medium transition ${loading
+            ? "bg-gray-400 cursor-not-allowed"
+            : "bg-blue-600 hover:bg-blue-700"
+            }`}
+        >
+          {loading ? "Logging in..." : "Login"}
+        </button>
+
+        <p className="text-center mt-4 text-gray-600">
+          Don't have an account?{" "}
+          <a href="/register" className="text-blue-500 hover:underline">
+            Sign up
+          </a>
+        </p>
+      </form>
+
+
+    </div>
+  );
 }
 
-export default login
+export default Login;
